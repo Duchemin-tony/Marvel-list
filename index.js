d9237055a3a0d69d8bcd12c85a8b1fe00f1a5818
Tony <duchemin.tony.dev@gmail.com>
2022-12-31 13:35:07 +0100

    Marvel API connection and loop

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const express = require('express')
 const path = require('path')
+const fetch = require('node-fetch')
 const { engine } = require('express-handlebars')
+const crypto = require('crypto');
 
 const PORT = process.env.PORT || 5003
 
 const app = express()
 
+let tskey = "1";
+let apikey = "1b04417e771e0c750cc451a75677f17f"
+let privatekey = "11119f6b85d4961d4407bbb84bffc070"
+
+const getAllCharacters = async () => {
+    try {
+        const res = await fetch('https://gateway.marvel.com:443/v1/public/characters?ts=' + tskey + '&apikey=' + apikey + '&hash=' + privatekey + '')
+        const json = await res.json()
+        return json
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')))
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
 
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
+app.get('/', async (_, res) => {
+    try {
+        const characters = await getAllCharacters()
+        res.render('Home', { characters })
+    } catch (err) {
+        console.log(err)
+    }
+})
 
-app.get('/', (_, res) =>
-    res.render('home', { title: 'Home', content: 'Le contenu de ma page' })
-)
+app.get('/:title', (req, res) => {
+    const title = req.params.title
+    res.render('profile', { title, content: `Ma page ${title}` })
+})
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
