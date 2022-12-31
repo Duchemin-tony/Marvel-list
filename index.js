c634f60d9ec8c474aad2f890dc40021a4375fa1f
Tony <duchemin.tony.dev@gmail.com>
2022-12-31 15:11:07 +0100

    Add High Order Function

--- index.js
+++ index.js
@@ -12,30 +12,24 @@ let tskey = "1";
 let apikey = "1b04417e771e0c750cc451a75677f17f"
 let privatekey = "11119f6b85d4961d4407bbb84bffc070"
 
-const getAllCharacters = async () => {
-    try {
-        const res = await fetch('https://gateway.marvel.com:443/v1/public/characters?ts=' + tskey + '&apikey=' + apikey + '&hash=' + privatekey + '')
+const catchErrors = asyncFunction => (...args) => asyncFunction(...args).catch(console.error)
+
+const getAllCharacters = catchErrors(async () => {
+        const res = await fetch('https://gateway.marvel.com:443/v1/public/characters?orderBy=modified&limit=50?&ts=' + tskey + '&apikey=' + apikey + '&hash=' + privatekey + '')
+        console.log(res)
         const json = await res.json()
         return json
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-}
+})
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')))
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
-app.get('/', async (_, res) => {
-    try {
+app.get('/', catchErrors(async (_, res) => {
         const characters = await getAllCharacters()
         res.render('Home', { characters })
-    } catch (err) {
-        console.log(err)
-    }
-})
+}))
 
 app.get('/:title', (req, res) => {
     const title = req.params.title
